3de62cac23b3d88eb2b276ec6f8e9f8f98ab3fb6
khvanches <khvanches@gmail.com>
2022-06-02 19:17:30 +0300

    Update test case's name

--- simple_shop_test/test_items.py
+++ simple_shop_test/test_items.py
@@ -5,7 +5,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 @pytest.mark.smoke
-def test_smoke(browser):
+def test_find_basket_button(browser):
     browser.get(link)
     basket_button = browser.find_element(By.CLASS_NAME, "add-to-basket")
     assert basket_button.is_enabled()
